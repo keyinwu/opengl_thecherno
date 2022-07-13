427d43f953353a422fe7e37001d6c6a0fe68a19d
keyinwu <43900343+keyinwu@users.noreply.github.com>
2022-07-13 14:51:18 +0800

    ep17

---
+++ src/17_textures/Shader.h
@@ -0,0 +1,43 @@
+#ifndef SHADER_H__
+#define SHADER_H__
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+
+
+using namespace std;
+
+struct ShaderProgramSource {
+    string VertexSource;
+    string FragmentSource;
+};
+
+class Shader
+{
+private:
+    string m_FilePath;
+    unsigned int m_RendererID;
+    unordered_map<string, int> m_UniformLocationCache;
+public:
+    Shader(const string& filepath);
+    ~Shader();
+
+    void Bind() const;
+    void Unbind() const;
+
+    // Set uniforms
+    void SetUniform1i(const string& name, int value);
+    void SetUniform1f(const string& name, float value);
+    void SetUniform4f(const string& name, float v0, float v1, float v2, float v3);
+private:
+    ShaderProgramSource ParseShader(const string& filepath);
+    unsigned int CompileShader(unsigned int type, const string& source);
+    unsigned int CreateShader(const string& vertexShader, const string& fragmentShader);
+
+    int GetUniformLocation(const string& name);
+};
+
+#endif
---
+++ src/17_textures/Utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H__
+#define UTILS_H__
+
+#define ASSERT(x) if (!(x)) assert(false)
+
+#endif
