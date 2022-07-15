07af39105d60f5777b1404bd8b259290db9aafca
keyinwu <43900343+keyinwu@users.noreply.github.com>
2022-07-15 15:03:29 +0800

    ep20

---
+++ src/20_projection_matrices/application.cpp
@@ -0,0 +1,169 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+using namespace std;
+
+// window settings
+const unsigned int SCR_WIDTH = 1024;
+const unsigned int SCR_HEIGHT = 768;
+
+// void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main() 
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    if( !glfwInit())
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glfwSwapInterval(1);
+
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // vertex buffers
+    // --------------
+    // {    /* Scope */
+        float positions[] = {
+            -0.5f, -0.5f, 0.0f, 0.0f,   // vertex0, texcoord
+             0.5f, -0.5f, 1.0f, 0.0f,
+             0.5f,  0.5f, 1.0f, 1.0f,
+            -0.5f,  0.5f, 0.0f, 1.0f
+        };
+
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0
+        };
+
+        // transparency
+        GLCALL(glEnable(GL_BLEND));
+        GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
+
+        IndexBuffer ib(indices, 6);
+
+        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);  // 4 : 3
+        // glm::mat4 proj = glm::ortho(0.0f, 1024.0f, 0.0f, 768.0f, -1.0f, 1.0f);
+
+        // vertex & fragment shaders
+        // -------------------------
+        Shader shader("res/shaders/matrix.shader");
+        shader.Bind();
+        Texture texture("res/textures/haikyu.png");
+        texture.Bind();                         // slot 0 by default
+        shader.SetUniform1i("u_Texture", 0);    // slot 0
+        shader.SetUniformMat4f("u_MVP", proj);  // model view projection matrix
+
+        // unbind
+        va.Unbind();
+        vb.Unbind();
+        ib.Unbind();
+        shader.Unbind();
+
+        float r = 0.0f;
+        float increment = 0.05f;
+
+        Renderer renderer;
+        renderer.BgColor(0.2f, 0.4f, 0.2f, 0.0f);
+
+        // render loop
+        // -----------
+        while (!glfwWindowShouldClose(window))
+        {
+            processInput(window);
+
+            renderer.Clear();
+
+            // rebind everything
+            renderer.Draw(va, ib, shader);
+
+            if (r > 1.0f)
+                increment = -0.05f;
+            if (r < 0.0f)
+                increment = 0.05f;
+            
+            r += increment;
+            
+            // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+        // glfw: terminate, clearing all previously allocated GLFW resources.
+        // ------------------------------------------------------------------
+    // }    /* Scope */
+
+    glfwTerminate();
+    return 0;
+}
+
+
+// process whether relevant keys are pressed/released
+// --------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// executes whenever the window size changed
+// -----------------------------------------
+// void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+// {
+//     GLCALL(glViewport(0, 0, width, height));
+// }
